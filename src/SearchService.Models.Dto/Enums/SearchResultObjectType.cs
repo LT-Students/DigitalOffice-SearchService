e0641dd4d7c90059916658c1dee8fa7c02617bb7
Igorok <fdiii@mail.ru>
2021-07-21 13:16:52 +0300

    init

--- src/SearchService.Models.Dto/Enums/SearchResultObjectType.cs
+++ src/SearchService.Models.Dto/Enums/SearchResultObjectType.cs
@@ -1,5 +1,9 @@
-﻿namespace LT.DigitalOffice.SearchService.Models.Dto.Enums
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace LT.DigitalOffice.SearchService.Models.Dto.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum SearchResultObjectType
     {
         User,
