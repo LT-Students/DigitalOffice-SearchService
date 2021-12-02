7656107ff92e79d70d8d3bdf17dc77d2adb12f4c
Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
2021-12-02 11:12:47 +0300

    update kernel (#14)
    * update kernel
    
    * fix tests

--- src/SearchService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/SearchService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,12 +1,12 @@
-﻿using LT.DigitalOffice.Kernel.Attributes;
-using LT.DigitalOffice.Kernel.Configurations;
-using LT.DigitalOffice.Models.Broker.Requests.Company;
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Requests.Department;
 using LT.DigitalOffice.Models.Broker.Requests.Project;
 using LT.DigitalOffice.Models.Broker.Requests.User;
 
 namespace LT.DigitalOffice.SearchService.Models.Dto.Configurations
 {
-    public class RabbitMqConfig : BaseRabbitMqConfig
+  public class RabbitMqConfig : BaseRabbitMqConfig
     {
         [AutoInjectRequest(typeof(ISearchProjectsRequest))]
         public string SearchProjectsEndpoint { get; set; }
